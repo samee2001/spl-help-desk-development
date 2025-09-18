5ec6d0c6b0caba7f173450e7fccff51cf38f52f9
Sameera Wijesekara <sameerawijesekara2001@gmail.com>
2025-09-18 17:55:30 +0530

    security change

--- configs/email_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Email Server Settings
-define('SMTP_HOST', 'splmail.sadaharitha.com');
-define('SMTP_PORT', 587);
-define('SMTP_USERNAME', 'ithelpdesk@sdhgroup.com'); // Use the provided username
-define('SMTP_PASSWORD', 'HelpIt#2025'); // Use the provided password
-define('SMTP_FROM_EMAIL', 'ithelpdesk@sadaharitha.com'); // The sender's email address
-define('SMTP_FROM_NAME', 'Sadaharitha IT Help Desk'); // Sender's name
